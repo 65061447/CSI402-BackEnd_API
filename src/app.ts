bb6ff85c2052dbfc5514fde9327da36457a4e374
65061447 <smith.dip@spumail.net>
2025-02-09 14:07:31 +0700

    set up

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express,{Request,Response} from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+ console.log(`Server is listening on port ${PORT}`);
+});
+
+
+
+
+app.use("/", (req: Request, res: Response) => {
+    res.send("Welcome");
+   });
